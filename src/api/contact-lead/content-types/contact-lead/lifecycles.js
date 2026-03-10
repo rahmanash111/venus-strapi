3ab0b7556cab19ff46db44dc660e55a3ce3f7f2d
rahman <rahmanash111>
2026-03-10 14:12:41 +0530

    fixed the mail trigger error

--- src/api/contact-lead/content-types/contact-lead/lifecycles.js
+++ src/api/contact-lead/content-types/contact-lead/lifecycles.js
@@ -1,13 +1,16 @@
 'use strict';
+
 const adminEmail = process.env.ADMIN_EMAIL;
 
 module.exports = {
     async afterCreate(event) {
         const { result } = event;
+
         try {
             await strapi.plugin('email').service('email').send({
                 to: adminEmail,
-                from: 'no-reply@venusinterior.com',
+                from: `Venus Interior <${adminEmail}>`,
+                replyTo: result.email,
                 subject: `New Lead From Venus Interior Website`,
                 text: `You have a new message from ${result.email}`,
                 html: `
@@ -18,7 +21,7 @@ module.exports = {
           <p><strong>Email:</strong> ${result.email || 'No Email'}</p>
           <p><strong>Phone Number:</strong> ${result.phone || 'No Phone'}</p>
           <p><strong>LeadType:</strong> ${result.leadType || 'No Subject'}</p>
-          <p><strong>Quote Details:</strong> ${result.quoteDetails || 'No Subject'}</p>
+          <p><strong>Quote Details:</strong> ${result.quoteDetails || 'No Details'}</p>
           <p><strong>Selected Package:</strong> ${result.selectedPackage || 'No Package Selected'}</p>
           <p><strong>Date & Time:</strong> ${result.entryDateAndTime}</p>
           <p><strong>Property Name:</strong> ${result.propertyName || 'No Property Name'}</p>
@@ -26,9 +29,40 @@ module.exports = {
         `,
             });
 
-            strapi.log.info('Contact email sent successfully');
+            if (result.email) {
+                await strapi.plugin('email').service('email').send({
+                    to: result.email, 
+                    from: `Venus Interior <${adminEmail}>`,
+                    replyTo: adminEmail, 
+                    subject: `We received your request | Venus Interior - Chennai`,
+                    text: `Hi ${result.name || 'Customer'}, we received your request. Our team will contact you shortly.`,
+                    html: `
+            <h2>Thank You for Contacting Venus Interior</h2>
+
+            <p>Hi <strong>${result.name || 'Customer'}</strong>,</p>
+
+            <p>We have successfully received your request.</p>
+
+            <p>Our team will contact you shortly.</p>
+
+            <h4>Your Submitted Details</h4>
+            <p><strong>Name:</strong> ${result.name && result.name.toUpperCase() || 'Not Provided'}</p>
+            <p><strong>Phone:</strong> ${result.phone || 'Not Provided'}</p>
+            <p><strong>Email:</strong> ${result.email || 'Not Provided'}</p>
+            <br/>
+
+            <p>Best Regards,</p>
+            <p><strong>Venus Interior Team</strong></p>
+            <p>📞 +91-9962993939</p>
+            <p>🌐 https://venusinterior.com</p>
+          `,
+                });
+            }
+
+            strapi.log.info('Admin email and user confirmation email sent successfully');
+
         } catch (err) {
-            strapi.log.error('Failed to send contact email:', err);
+            strapi.log.error('Failed to send email:', err);
         }
     },
 };
